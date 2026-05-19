40c828952f4283a3a9d129b2e683f8e2fd6faeb1
saint-cc <saint@cyberchaos.nl>
2026-05-19 13:01:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# meshchat
+mesh chat
---
+++ static/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",()=>{console.log("SW installed");});self.addEventListener("fetch",event=>{event.respondWith(fetch(event.request));});
